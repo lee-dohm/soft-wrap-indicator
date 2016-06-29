6ff48347664962144bfafb37da724d8a815a7613
Lee Dohm <lee@lee-dohm.com>
2016-06-28 18:27:50 -0700

    :green_heart: Get some tests off the ground

---
+++ test/mock-editor.js
@@ -0,0 +1,28 @@
+'use babel'
+
+export default class MockEditor {
+  constructor (softWrap) {
+    this.softWrap = softWrap
+    this.toggled = false
+  }
+
+  onDidChangeGrammar () {
+    return new CompositeDisposable()
+  }
+
+  onDidChangeSoftWrapped () {
+    return new CompositeDisposable()
+  }
+
+  onDidDestroy () {
+    return new CompositeDisposable()
+  }
+
+  isSoftWrapped () {
+    return this.softWrap
+  }
+
+  toggleSoftWrapped () {
+    this.toggled = true
+  }
+}
